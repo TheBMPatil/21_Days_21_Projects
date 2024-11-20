c8d9ef7278f12fdaae833d6040218dcf8e518d96
Bhagvat Mutthe <bhagvatmutthe6@gmail.com>
2024-11-20 17:26:15 +0530

    Screnshots added

--- Day03/weather_app/src/App.jsx
+++ Day03/weather_app/src/App.jsx
@@ -5,12 +5,10 @@ import Weather from './components/weather'
 const App = () => {
   return (
     <div className='app'>
-      
-      <Weather>
 
-      </Weather>
-      
-      </div>
+      <Weather />
+
+    </div>
   )
 }
 
